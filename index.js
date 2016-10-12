4bb59a13ae0071e739dbc5cfb19e9678d761831a
Banks Humphreys <iobg0320@gmail.com>
2016-10-12 12:29:37 -0500

    getting mouse position with robot

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict'
+const EventEmitter = require('events')
+const express = require('express')
+const app = express()
+const emitter = new EventEmitter()
+const robot = require('robot-js')
+
+var stdin = process.stdin;
+
+// without this, we would only get streams once enter is pressed
+stdin.setRawMode( true );
+
+// resume stdin in the parent process (node app won't quit all by itself
+// unless an error or process.exit() happens)
+stdin.resume();
+
+// i don't want binary, do you?
+stdin.setEncoding( 'utf8' );
+
+// on any data into stdin
+stdin.on( 'data', function( key ){
+  // ctrl-c ( end of text )
+  console.log(robot.Mouse.getPos())
+  if ( key === '\u0003' ) {
+    process.exit();
+  }
+  // write the key to stdout all normal like
+  process.stdout.write( key );
+});
